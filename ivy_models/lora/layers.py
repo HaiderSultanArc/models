ba69278212a0b707a05963141fda23e314fd29b9
Haider Sultan  ⚡ <haidersultanarc@gmail.com>
2023-08-10 18:25:10 +0000

    LoRA implementation

--- ivy_models/lora/layers.py
+++ ivy_models/lora/layers.py
@@ -68,7 +68,7 @@ class Embedding(ivy.Embedding, LoRALayer):
                     self._embedding_dim,
                     self.r,
                     dtype,
-                )
+                ),
             )
         self.reset_parameters()
         return self.v
@@ -161,7 +161,7 @@ class Linear(ivy.Linear, LoRALayer):
                     self._out_features,
                     self.r,
                     dtype,
-                )
+                ),
             )
         self.reset_parameters()
         return self.v
@@ -279,7 +279,7 @@ class MergedLinear(ivy.Linear, LoRALayer):
                     device,
                     self._output_channels,
                     dtype=ivy.bool,
-                ).reshape(len(self.enable_lora), -1)
+                ).reshape(len(self.enable_lora), -1),
             )
         self.reset_parameters()
         return self.v
@@ -325,7 +325,7 @@ class MergedLinear(ivy.Linear, LoRALayer):
                     self.v.w += self.merge_AB() * self.scaling
                 self.merged = True
 
-    def forward(self, x: ivy.Array):
+    def _forward(self, x: ivy.Array):
         def T(w):
             return w.transpose(0, 1) if self.fan_in_fan_out else w
 
@@ -336,3 +336,136 @@ class MergedLinear(ivy.Linear, LoRALayer):
             if self.r > 0:
                 result += self.lora_dropout(x) @ T(self.merge_AB().T) * self.scaling
             return result
+
+
+class ConvLoRA(ivy.Module, LoRALayer):
+    def __init__(
+        self,
+        conv_module,
+        in_channels,
+        out_channels,
+        kernel_size,
+        r=0,
+        lora_alpha=1,
+        lora_dropout=0.0,
+        merge_weights=True,
+        **kwargs
+    ):
+        super(ConvLoRA, self).__init__()
+        self.conv = conv_module(
+            in_channels,
+            out_channels,
+            kernel_size,
+            weight_initializer=ivy.Zeros,
+            **kwargs,
+        )
+        LoRALayer.__init__(
+            self,
+            r=r,
+            lora_alpha=lora_alpha,
+            lora_dropout=lora_dropout,
+            merge_weights=merge_weights,
+        )
+        assert isinstance(kernel_size, int)
+
+        # Actual trainable parameters
+        if r > 0:
+            self.scaling = self.lora_alpha / self.r
+            # Freezing the pre-trained weight matrix
+            # self.conv.weight.requires_grad = False
+        self.reset_parameters()
+        self.merged = False
+
+    def _create_variables(self, *, device=None, dtype=None):
+        if self.r > 0:
+            self.v = dict(
+                **self.v,
+                lora_A=self.conv._weight_initializer.create_variables(
+                    (
+                        self.r * self.conv.kernel_size[0],
+                        self.conv.in_channels * self.conv.kernel_size[0],
+                    ),
+                    device,
+                    self.r * self.conv.kernel_size[0],
+                    self.conv.in_channels * self.conv.kernel_size[0],
+                    dtype,
+                ),
+                lora_B=self.conv._weight_initializer.create_variables(
+                    (
+                        self.conv.out_channels
+                        // self.conv.groups
+                        * self.conv.kernel_size[0],
+                        self.r,
+                    ),
+                    device,
+                    self.conv.out_channels
+                    // self.conv.groups
+                    * self.conv.kernel_size[0],
+                    self.r,
+                    dtype,
+                ),
+            )
+        self.reset_parameters()
+        return self.v
+
+    def reset_parameters(self):
+        if hasattr(self, "v"):
+            # initialize A the same way as the default for ivy.Linear and B to zero
+            ivy.inplace_update(
+                self.v.lora_A, ivy.kaiming_uniform_(self.v.lora_A, a=ivy.sqrt(5))
+            )
+            ivy.inplace_update(self.v.lora_B, ivy.zeros(self.v.lora_B))
+
+    def train(self, mode=True):
+        super(ConvLoRA, self).train(mode)
+        if mode:
+            if self.merge_weights and self.merged:
+                if self.r > 0:
+                    # Make sure that the weights are not merged
+                    self.conv.v.w -= (
+                        ivy.reshape(
+                            (self.v.lora_B @ self.v.lora_A), self.conv.v.w.shape
+                        )
+                        * self.scaling
+                    )
+                self.merged = False
+        else:
+            if self.merge_weights and not self.merged:
+                if self.r > 0:
+                    # Merge the weights and mark it
+                    self.conv.v.w += (
+                        ivy.reshape(
+                            (self.v.lora_B @ self.v.lora_A), self.conv.v.w.shape
+                        )
+                        * self.scaling
+                    )
+                self.merged = True
+
+    def _forward(self, x):
+        if self.r > 0 and not self.merged:
+            return self.conv._conv_forward(
+                x,
+                self.conv.v.w
+                + ivy.reshape((self.v.lora_B @ self.v.lora_A), self.conv.v.w.shape)
+                * self.scaling,
+                self.conv.v.b,
+            )
+        return self.conv(x)
+
+
+class Conv2D(ConvLoRA):
+    def __init__(self, *args, **kwargs):
+        super(Conv2D, self).__init__(ivy.Conv2D, *args, **kwargs)
+
+
+class Conv1D(ConvLoRA):
+    def __init__(self, *args, **kwargs):
+        super(Conv1D, self).__init__(ivy.Conv1D, *args, **kwargs)
+
+
+# Can Extend to other ones like this
+
+
+class Conv3D(ConvLoRA):
+    def __init__(self, *args, **kwargs):
+        super(Conv3D, self).__init__(ivy.Conv3D, *args, **kwargs)
